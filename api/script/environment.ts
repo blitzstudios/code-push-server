9835486fc528712675173ca513f1695b0705e60e
Luke Herbold <luke@sleeper.app>
2025-02-07 16:41:25 -0800

    Add fallback temp directory for codepush release storage.

--- api/script/environment.ts
+++ api/script/environment.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import * as os from 'os';
+
 export function getTempDirectory(): string {
-  return process.env.TEMP || process.env.TMPDIR;
+  return process.env.TEMP || process.env.TMPDIR || os.tmpdir();
 }
